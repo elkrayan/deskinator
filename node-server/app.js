6ba1a697c403508f1c10f182dbdf7d99b758c0dc
Rayan El-Koukouchi <rayan.elk@hotmail.com>
2022-01-24 07:24:21 +0100

    Ready for test

--- node-server/app.js
+++ node-server/app.js
@@ -19,17 +19,17 @@ io.on('connection', (socket) => {
     socket.on('deskCall', (value) => {
         console.log('\n---------- NEW CALL ----------');
         console.log(`Desk ${value} is calling a new patient`);
-        // TODO: Check if value already in waitList
-        waitList.unshift(value);
-        console.log(`Actual waiting list ${waitList}`);
+    
+        // Send to public
+        io.emit('toPublic', value);
 
     });
     socket.on('arrivedDesk', (value) => {
         console.log('\n---------- ARRIVED ----------');
         console.log(`Patient at Desk ${value} is arrived`);
-        let index = waitList.indexOf(value);
-        waitList.splice(index)
-        console.log(`Actual waiting list ${waitList}`);
+        
+        // Send to public
+        io.emit('removreFromList', value);
     })
 })
 
--- screen/src/js/app.js
+++ screen/src/js/app.js
@@ -1,31 +1,73 @@
 "use strict";
+// SOCKET.IO
 
 window.onload = async () => {
     await getCurrentWeather('brussels');
     document.querySelector('#date').innerHTML = getDate();
-    setInterval( () => {
+    setInterval(() => {
         document.querySelector('#time').innerHTML = getHour();
+    });
+
+    let messages = ["Hello", "World", "Forest", "Covid"];
+    document.getElementById('messageModule').innerHTML = messages[0];
+    messageModule(messages);
+
+    // SOCKET.IO
+    const socket = io('http://localhost:3000');
+    let waitingList = [];
+    let deskDisplay = document.getElementById('desk').children;
+    socket.on('toPublic', (desk) => {
+        if (!waitingList.includes(desk)) {
+            let p = document.createElement('p');
+            p.textContent = desk;
+            p.setAttribute('data-desk', desk);
+            p.classList.add('active');
+            document.getElementById('desk').append(p);
+
+            document.getElementById('sound').play()
+
+            waitingList.push(desk);
+
+            if(waitingList.length >= 6){
+                let index = waitingList.indexOf(deskDisplay[0].getAttribute('data-desk'));
+                waitingList.splice(index, 1);
+                deskDisplay[0].remove();
+                console.log(index, waitingList);
+            }
+        }
+    })
+    socket.on('removreFromList', (value) => {
+        let index = waitingList.indexOf(value);
+        waitingList.splice(index, 1);
+
+        let displayDesk = document.getElementById('desk').children;
+        for (let o of displayDesk) {
+            if (o.getAttribute('data-desk') == value)
+                o.classList.remove('active');
+        }
     })
 }
 
 
 
 
-
-function getHour(){
+function getHour() {
     function addZero(i) {
-        if (i < 10) {i = "0" + i}
+        if (i < 10) {
+            i = "0" + i
+        }
         return i;
-      }
-      
-      const d = new Date();
-      let h = addZero(d.getHours());
-      let m = addZero(d.getMinutes());
-      let s = addZero(d.getSeconds());
-      let time = h + ":" + m + ":" + s;
-      return time;
+    }
+
+    const d = new Date();
+    let h = addZero(d.getHours());
+    let m = addZero(d.getMinutes());
+    let s = addZero(d.getSeconds());
+    let time = h + ":" + m + ":" + s;
+    return time;
 }
-function getDate(){
+
+function getDate() {
     let d = new Date();
     let dayFr = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
     let monthFr = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septemblre', 'octobre', 'novembre', 'décembre'];
@@ -44,16 +86,18 @@ let getCurrentWeather = async (city) => {
 }
 
 let displatWeather = (data) => {
-    console.log(data)
-    let temps = [`${Math.round(data.main.temp_min)}°C`,`${Math.round(data.main.temp_max)}°C`,`${Math.round(data.main.feels_like)}°C`];
+    let temps = [`${Math.round(data.main.temp_min)}°C`, `${Math.round(data.main.temp_max)}°C`, `${Math.round(data.main.feels_like)}°C`];
     let currentTemp = `${Math.round(data.main.temp)}°C`;
 
     document.querySelector("html body main div.row.middle-module div.col.half div.module.full.weather.col div.row div.now-weather p")
         .innerHTML = currentTemp;
-    
+    for (let weather of data.weather) {
+        document.getElementById('weatherImg').setAttribute('src', `http://openweathermap.org/img/wn/${weather.icon}@2x.png`);
+    }
+
     let count = 0;
     let text = ['min', 'max', 'ressenti'];
-    for (let temp of temps){
+    for (let temp of temps) {
         let el = document.createElement('li');
         el.textContent = `${text[count]}: ${temp}`;
         document.getElementById('weatherDetail').append(el);
@@ -61,25 +105,11 @@ let displatWeather = (data) => {
     }
 
 }
-/* let displayCurrentWeather = (data) => {
-    // http://openweathermap.org/img/wn/10d@2x.png
-    console.log(data);
-    let temps = [`${Math.round(data.main.temp_min)}°C`,`${Math.round(data.main.temp_max)}°C`,`${Math.round(data.main.feels_like)}°C`];
-    let currentTemp = `${Math.round(data.main.temp)}°C`;
-    document.getElementById('currentTemp').innerHTML = currentTemp;
-    for (let temp of temps){
-        let el = document.createElement('h3');
-        el.textContent = temp;
-        document.getElementById('tempInfo').append(el);
-    }
-
-    for (let weather of data.weather){
-        let icon = `http://openweathermap.org/img/wn/${weather.icon}@2x.png`;
-        let img = document.createElement('img');
-        img.setAttribute('src', icon);
-        document.getElementById('weatherIcon').append(img);
-
-        let description = weather.description;
-    }
-
-} */
+let messageModule = (messages) => {
+    let messageCount = 1;
+    setInterval(() => {
+        document.getElementById('messageModule').innerHTML = messages[messageCount++]
+        if (messageCount >= messages.length)
+            messageCount = 0;
+    }, 5000)
+}
